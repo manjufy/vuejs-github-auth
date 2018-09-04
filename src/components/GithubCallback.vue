8e8f3710ff8bddba4dcc1ed5823a4c08e0f07648
Manjunath Reddy <manju16832003@gmail.com>
2018-09-04 21:53:11 +0800

    Remove console logs

--- src/components/GithubCallback.vue
+++ src/components/GithubCallback.vue
@@ -18,7 +18,6 @@ export default {
     const code = this.$route.query.code
     console.log('Calling API to get the access token.')
     axios.get(`http://dev.githubauth.com:3333/auth?code=${code}`).then(data => {
-      console.log('Access token', data.data.access_token)
       // call github to get user info
       axios.get('https://api.github.com/user', {
           params: {
